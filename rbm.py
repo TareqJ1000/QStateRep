a710b57047b3b489cf8c4a5769c9780b10d5fe7d
Tareq Jaouni <tjaou104@uottawa.ca>
2025-04-09 17:53:14 -0400

    initial commit

---
+++ rbm.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+'''
+rbm.py 
+
+Loads the class which implements a Restricted Boltzmann Machine (RBM).  
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Let's define the sigmoid function. The Gibbs sampler thingy is predictated on computing conditional probabilities. 
+
+class ResBoltMan():
+    def __init__(self, num_visi, num_hid): 
+        
+        self.num_visi = num_visi
+        self.num_hid = num_hid
+        self.hid = np.random.randint(0, 2, size=num_hid)
+        
+        #self.visi = np.random.randint(0, 2, size=num_visi)
+        self.weights = np.random.uniform(-0.05,0.05, size=(num_visi, num_hid)) 
+        self.bias_visi = np.random.uniform(0, 0.05, size=num_visi)
+        self.bias_hid = np.random.uniform(0, 0.05, size=num_hid)
+        
+    def sigmoid(self, z):
+        return 1/(1 + np.exp(-z))
+    
+    def prob_function(self, visi, hid): # This computes p(v,h). We assume that v and h are seperate neuron configurations  
+        total_prob = 0 
+        for ii in range(self.num_visi):
+            for jj in range(self.num_hid):
+                first_term= visi[ii]@self.weights[ii, jj]@hid[jj]
+                second_term = visi[ii]@self.bias_visi[ii]
+                third_term = hid[jj]@self.bias_hid[jj]
+        
+        return np.exp(first_term + second_term + third_term)
+    
+    def unnorm_prob_function(self, visi, hid_list): # Given a batch of visible neuron configuartions, this computes sum_(p(v,h))
+        total_prob = 0
+        for hid in hid_list: 
+            total_prob += self.prob_function(visi, hid)
+            
+        return total_prob
+    
+    def grad_update(self, visi_list_0, hid_list_0, visi_list_1, hid_list_1, lr): # This computes the expectation values of quantities necessary to update the parameters
+        
+        for i in range(self.num_visi):
+            for j in range(self.num_hid):
+                self.weights[i,j] += lr*np.mean(visi_list_0[:, i]*hid_list_0[:, j] - visi_list_1[:, i]*hid_list_1[:, j])       
+                                                
+        for i in range(self.num_visi):
+            self.bias_visi[i] += lr*np.mean(visi_list_0[:, i] - visi_list_1[:,i])
+            
+        for j in range(self.num_hid):
+            self.bias_hid[j] += lr*np.mean(hid_list_0[:, j] - hid_list_1[:, j])
+            
+        
+    def v_given_h(self, i):
+        # This function computes the conditional probability that the visible neuron is equal to 1
+        # b_v -> visible neuron bias
+        # W -> weight vector of RBM 
+        # h -> hidden neuron vector 
+        # i -> i_th visible neuron
+    
+        sum_over_hidden = np.sum(np.array([self.weights[i,j]*self.hid[j] for j in range(self.num_hid)]))
+        
+        return self.sigmoid(self.bias_visi[i] + sum_over_hidden)
+    
+    def h_given_v(self, visi, j):
+        # This function computes the conditional probability that the hidden neuron is equal to 1
+        # b_h -> hidden neuron bias
+        # W -> weight vector of RBM 
+        # v -> input visible neuron vector 
+        # j -> j_th visible neuron
+        
+        sum_over_visible = np.sum(np.array([self.weights[i,j]*visi[i] for i in range(self.num_visi)]))
+        return self.sigmoid(self.bias_hid[j] + sum_over_visible) 
+    
+    def sample_new_v(self):
+        # Samples a new visible vector given hidden neuron configuration 
+        # v - visible neuron vector
+        # h - hidden neuron vector 
+        # W - weight vector
+        # b_v - visible bias vector 
+    
+        # Let's instantiate a copy of the visible neuron vector 
+        visi_neo = np.zeros(self.num_visi)
+    
+        # Let's create a series of uniformly distributed random values between 0 and 1 
+        rand_visi = np.random.rand(self.num_visi)
+    
+        # The conditional probability defines the threshold. If it's less than or equal to this threshold, set the corresponding visible vector to 1. 
+        
+        for ii in range(self.num_visi):
+            if (rand_visi[ii] < self.v_given_h(ii)):
+                visi_neo[ii] = 1
+            else:
+                visi_neo[ii] = 0 
+
+        return visi_neo 
+    
+        #self.visi = visi_neo
+    
+    def sample_new_h(self, visi): 
+        
+        # Samples a new visible vector given hidden neuron configuration 
+        # v - visible neuron vector
+        # h - hidden neuron vector 
+        # W - weight vector
+        # b_v - visible bias vector 
+    
+        # Let's instantiate a copy of the visible neuron vector 
+        hid_neo = np.zeros(self.num_hid)
+    
+        # Let's create a series of uniformly distributed random values between 0 and 1 
+        rand_visi = np.random.rand(self.num_hid)
+    
+        # The conditional probability defines the threshold. If it's less than or equal to this threshold, set the corresponding visible vector to 1. 
+        for jj in range(self.num_hid):
+            if (rand_visi[jj] < self.h_given_v(visi, jj)):
+                hid_neo[jj] = 1
+            else:
+                hid_neo[jj] = 0 
+        
+        self.hid = hid_neo
+        
